b6fd1b9c69f8d9011535902ef55947f66c1ccfff
Andrés Oliveira <andres.oliveira@zeeng.com.br>
2022-10-19 15:17:07 -0300

    Financee Card List

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-import { Container } from './styles'
+import { Container, Content } from './styles'
 
 import ContentHeader from '../../components/ContentHeader'
 import SelectInput from '../../components/SelectInput'
+import HistoryFinanceCard from '../../components/HistoryFinanceCard'
 
 const List: React.FC = () => {
 
@@ -15,9 +16,87 @@ const List: React.FC = () => {
   ]
 
   return (
-    <ContentHeader title="Saídas" lineColor="#e44c4e">
-      <SelectInput options={options}/>
-    </ContentHeader>
+    <Container>
+      <ContentHeader title="Saídas" lineColor="#e44c4e">
+        <SelectInput options={options}/>
+      </ContentHeader>
+
+      <Content>
+        <HistoryFinanceCard 
+          cardColor="#313862"
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="27/07/2020"
+          amount="R$ 130,00"
+        />
+
+        <HistoryFinanceCard 
+          cardColor="#313862"
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="27/07/2020"
+          amount="R$ 130,00"
+        />
+
+        <HistoryFinanceCard 
+          cardColor="#313862"
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="27/07/2020"
+          amount="R$ 130,00"
+        />
+
+        <HistoryFinanceCard 
+          cardColor="#313862"
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="27/07/2020"
+          amount="R$ 130,00"
+        />
+
+        <HistoryFinanceCard 
+          cardColor="#313862"
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="27/07/2020"
+          amount="R$ 130,00"
+        />
+
+        <HistoryFinanceCard 
+          cardColor="#313862"
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="27/07/2020"
+          amount="R$ 130,00"
+        />
+
+        <HistoryFinanceCard 
+          cardColor="#313862"
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="27/07/2020"
+          amount="R$ 130,00"
+        />
+
+        <HistoryFinanceCard 
+          cardColor="#313862"
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="27/07/2020"
+          amount="R$ 130,00"
+        />
+
+        <HistoryFinanceCard 
+          cardColor="#313862"
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="27/07/2020"
+          amount="R$ 130,00"
+        />
+
+        
+      </Content>
+    </Container>
   )
 }
 
