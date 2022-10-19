e4e6cdf41b2ebf5a8ef47251abd10ac01dd1307e
Andrés Oliveira <andres.oliveira@zeeng.com.br>
2022-10-19 18:02:09 -0300

    Filters

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Container, Content } from './styles'
+import { Container, Content, Filters } from './styles'
 
 import ContentHeader from '../../components/ContentHeader'
 import SelectInput from '../../components/SelectInput'
@@ -8,18 +8,40 @@ import HistoryFinanceCard from '../../components/HistoryFinanceCard'
 
 const List: React.FC = () => {
 
-  const options = [
-    {value: 'Andres', label: 'Andres'},
-    {value: 'Amanda', label: 'Amanda'},
-    {value: 'Joao', label: 'Joao'},
-    {value: 'Silva', label: 'Silva'},
+  const months = [
+    {value: '7', label: 'Julho'},
+    {value: '8', label: 'Agosto'},
+    {value: '9', label: 'Setembro'},
+  ]
+
+  const years = [
+    {value: '2022', label: '2022'},
+    {value: '2021', label: '2021'},
+    {value: '2020', label: '2020'},
   ]
 
   return (
     <Container>
       <ContentHeader title="Saídas" lineColor="#e44c4e">
-        <SelectInput options={options}/>
+        <SelectInput options={months}/>
+        <SelectInput options={years}/>
       </ContentHeader>
+
+      <Filters>
+        <button 
+          type="button"
+          className='tag-filter tag-filter-recurrent'
+        >
+            Recorrentes
+        </button>
+
+        <button 
+          type="button"
+          className='tag-filter tag-filter-eventual'
+        >
+            Eventuais
+        </button>
+      </Filters>
 
       <Content>
         <HistoryFinanceCard 
